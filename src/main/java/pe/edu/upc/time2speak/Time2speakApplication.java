c98c08dd57dd2702d97ff34326bca8ea8c364a2c
Fernando Ponce <exminaruto@gmail.com>
2020-04-19 13:23:27 -0500

    Creacion de proyecto y de los paquetes

---
+++ src/main/java/pe/edu/upc/time2speak/Time2speakApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.time2speak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Time2speakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Time2speakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/time2speak/Time2speakApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.time2speak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Time2speakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
